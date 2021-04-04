9c43954127aac3b45d458cb8da6583ec4a3aec97
AkiRa <79385860+AkiRaID@users.noreply.github.com>
2021-04-04 22:36:57 +0700

    Add files via upload

---
+++ akiraganz/music.js
@@ -0,0 +1,4 @@
+[
+{"result": "https://open.spotify.com/track/6OrbbaqibURt0PGZvyFtyJ"},
+{"result": "https://open.spotify.com/track/7qEHsqek33rTcFNT9PFqLf"}
+]
---
+++ akiraganz/sewabot.js
@@ -0,0 +1,15 @@
+const sewabot = () => { 
+	return `
+*OPEN JASA SEWA BOT*
+
+Sewa Sebulan : 10k
+Sewa Permanen : 25k
+
+*Payment :*
+_Gopay, Dana,Qris dan Tsel_
+
+Minat?PC
+Wa.me/6282158549899
+`
+}
+exports.sewabot = sewabot
